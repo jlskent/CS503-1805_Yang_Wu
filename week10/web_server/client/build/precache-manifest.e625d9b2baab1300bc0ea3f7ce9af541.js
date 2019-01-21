e87c9b11a514831942f1681e378734031943a4b1
YangWu <wuyang@wustl.edu>
2019-01-21 15:13:46 -0600

    mess

---
+++ week10/web_server/client/build/precache-manifest.e625d9b2baab1300bc0ea3f7ce9af541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9ebcc13a446003855c54117e7f37b87",
+    "url": "/static/media/logo.f9ebcc13.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8aa02b8d4ddfd30c87cd",
+    "url": "/static/js/main.8aa02b8d.chunk.js"
+  },
+  {
+    "revision": "3ad7d1d9aea6f962bd0d",
+    "url": "/static/js/1.3ad7d1d9.chunk.js"
+  },
+  {
+    "revision": "8aa02b8d4ddfd30c87cd",
+    "url": "/static/css/main.bedc0df2.chunk.css"
+  },
+  {
+    "revision": "3ad7d1d9aea6f962bd0d",
+    "url": "/static/css/1.a787a4d7.chunk.css"
+  },
+  {
+    "revision": "715bb71b4b799bb5b98b0f42cd379375",
+    "url": "/index.html"
+  }
+];
